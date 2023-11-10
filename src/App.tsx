1e4b78b50fe77ef7ae8476a6d25ff8bc6b234d47
behong1999 <khonhong46632@gmail.com>
2023-11-10 23:04:02 +0200

    palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import './App.css';
 
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <h1 className='text-3xl font-bold underline text-dark_green-800'>
+      Hello world!
+    </h1>
+  );
 }
 
 export default App;
