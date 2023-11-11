8993e929a0dda817b2bf03932abbab9d80978774
Diddayngo <lamngo606@gmail.com>
2023-11-11 11:40:30 +0200

    merge app incompleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./auth/Login";
+
 function App() {
   return (
     <Router>
