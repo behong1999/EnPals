121dea5e78adc0f14697a98441d621b43df97e55
Hoang Duong <hoangduongphantri@gmail.com>
2023-11-11 14:55:15 +0200

    feat: create dashboard user-summary & price-statistics

---
+++ src/components/dashboard/UserSummary.tsx
@@ -0,0 +1,54 @@
+import {
+  BoltIcon,
+  CurrencyEuroIcon,
+  LightBulbIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline"
+
+const contentData = [
+  {
+    title: "Users",
+    icon: <UserIcon />,
+    value: "45",
+  },
+  {
+    title: "Last Month Consumption",
+    icon: <LightBulbIcon />,
+    value: "10.20e",
+  },
+  {
+    title: "Last Month Usage",
+    icon: <BoltIcon />,
+    value: "111 kWh",
+  },
+  {
+    title: "Total Saving",
+    icon: <CurrencyEuroIcon />,
+    value: "30",
+  },
+]
+
+const UserSummary = () => {
+  return (
+    <div className="flex flex-row justify-between w-full space-x-4 p-2">
+      {contentData.map((data) => (
+        <div
+          key={data.title}
+          className="flex flex-row content-center bg-gray-50 w-3/12 rounded-md p-3"
+        >
+          <div className="bg-gray-100 w-12 h-12 rounded-full p-3">
+            {data.icon}
+          </div>
+          <div className="ml-3">
+            <p className="font-bold	text-sm text-primary-900">{data.title}</p>
+            <h1 className="font-bold text-2xl text-primary-400">
+              {data.value}
+            </h1>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default UserSummary
