f77a14c8de9ea58113e35e3e0aa9f32831a33a9c
Hoang Duong <hoangduongphantri@gmail.com>
2023-11-11 17:05:01 +0200

    feat: Add table

--- src/components/dashboard/GeneralRankingTable.tsx
+++ src/components/dashboard/GeneralRankingTable.tsx
@@ -1,7 +1,43 @@
+import rankingData from "../../data/userRanking.json"
+
+const convertToEuros = (consumption: number): number => {
+  return Number.parseFloat(((consumption * 14.42) / 100).toFixed(2))
+}
+
 const GeneralRankingTable = () => {
+  const sortedData = [...rankingData].sort((prev, curr) =>
+    convertToEuros(prev.last_usage - prev.saving) / prev.number_of_members >
+    convertToEuros(curr.last_usage - prev.saving) / curr.number_of_members
+      ? 1
+      : -1
+  )
   return (
-    <div>
-      <table></table>
+    <div className="bg-gray-50 rounded-md p-5 w-full">
+      <h1 className="font-bold text-base text-primary-900">
+        Top Energy-saving Family
+      </h1>
+      <table className="w-full rounded-md">
+        <thead className="bg-gray-200">
+          <tr>
+            <th className="px-6 py-3">Name</th>
+            <th className="px-6 py-3">Last Month Spend/Usage</th>
+            <th className="px-6 py-3">Saving</th>
+            <th className="px-6 py-3">Rank</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedData.slice(0, 10).map((family, index) => (
+            <tr key={family.socialnumbers} className="border-b border-gray-200">
+              <td className="px-6 py-3">{family.name}</td>
+              <td className="px-6 py-3">
+                {convertToEuros(family.last_usage)}e/{family.last_usage} kWh
+              </td>
+              <td className="px-6 py-3">{convertToEuros(family.saving)}</td>
+              <td className="px-6 py-3">#{index + 1}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
+import EnergyResourcesChart from "../components/dashboard/EnergyResourcesChart"
 import ExchangePrice from "../components/dashboard/ExchangePrice"
+import GeneralRankingTable from "../components/dashboard/GeneralRankingTable"
 import UserSummary from "../components/dashboard/UserSummary"
 
 export default function Home() {
   return (
-    <div>
-      <UserSummary />
+    <div className="grid grid-cols-2 gap-4">
+      <div className="col-span-2">
+        <UserSummary />
+      </div>
       <ExchangePrice />
+      <EnergyResourcesChart />
+      <div className="col-span-2">
+        <GeneralRankingTable />
+      </div>
     </div>
   )
 }
