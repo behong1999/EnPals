8bfc2cc825fce7291e38d1df53a49c190f58c951
behong1999 <khonhong46632@gmail.com>
2023-11-11 18:01:57 +0200

    resolve conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './auth/Login';
 import Report from './pages/Report';
 import PieChart, { pieChartData } from './components/PieChart';
-
-=======
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Outlet,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./auth/Login";
 import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
+import Sidebar from './components/SideBar';
 
 const AppLayout = () => (
   <div className="max-h-screen flex flex-col">
@@ -29,12 +18,10 @@ const AppLayout = () => (
   </div>
 );
 
->>>>>>> 672991b84a02933ae6b3e14d9c2076f8719f17c1
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
         <Route path='/' element={<Home />} />
         <Route path='/report' element={<Report />} />
         <Route path='/login' element={<Login />} />
@@ -44,12 +31,10 @@ function App() {
             <PieChart labels={pieChartData.labels} data={pieChartData.data} />
           }
         />
-=======
         <Route path="/login" element={<Login />} />
         <Route element={<AppLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
->>>>>>> 672991b84a02933ae6b3e14d9c2076f8719f17c1
       </Routes>
     </Router>
   );
