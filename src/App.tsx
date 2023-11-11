cfe8de818d4ce1543d82d58fddb97ef0fc91e1bc
behong1999 <khonhong46632@gmail.com>
2023-11-11 16:59:48 +0200

    Revert "PaymentHistory components"
    This reverts commit 857f2157c3658085c750cbe8585e41319f1eef6c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./auth/Login";
+import PieChart, { pieChartData } from './components/PieChart';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/pieChart" element={<PieChart labels={pieChartData.labels} data={pieChartData.data} />} />
       </Routes>
     </Router>
   );
