cfa6c9df0c5ce49e18775a44e6063f1e9538f5b5
Diddayngo <lamngo606@gmail.com>
2023-11-11 16:33:13 +0200

    sibar and navbar completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./auth/Login";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <div className="max-h-screen flex flex-col">
+        <Header />
+        <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+          <Sidebar />
+          <div className="overflow-x-hidden px-8 pb-4">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </Router>
   );
 }
 
-export default App
+export default App;
